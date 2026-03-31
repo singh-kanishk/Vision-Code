69ace32fc8399e2bfe82f3c19d15e610dce07ee2
singh-kanishk <skanishk6124@gmail.com>
2026-03-31 23:33:56 +0530

    removed error on cloning because of build file

--- packages/shared/src/form/formSchema/room-form-schema.js
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-export const roomFormSchema = z.object({
-    roomId: z.string().min(3, "Room ID must be at least 3 characters").max(20),
-    passcode: z.string().min(4, "Passcode must be at least 4 characters"),
-});
